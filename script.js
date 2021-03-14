23bc71791d3bae6410b53c67b4611361d89a020c
vng <nguyenvohoang20061992@gmail.com>
2021-03-14 21:37:42 +0100

    add DOM variables and control quote contains

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
 // Global variables
+
+// DOM
+const mQuoteContainer = document.getElementById("quote-container");
+const mQuoteText      = document.getElementById("quote");
+const mAuthorText     = document.getElementById("author");
+const mAuthorButton   = document.getElementById("twitter");
+const mNewQuoteButton = document.getElementById("new-quote");
+
+// Quote attributes
 let mApiQuotes = [];
 
 // Show new quote
 function newQuote() {
     // Pick a random quote from mApiQuotes array
     const vQuote = mApiQuotes[Math.floor(Math.random() * mApiQuotes.length)];
-    console.log(vQuote);
+
+    // Check if author field is blank and replace it with "Unknown"
+    if  (vQuote.author == null) {
+        mAuthorText.textContent = "Unknown";
+    } else {
+        mAuthorText.textContent = vQuote.author;
+    }
+
+    // Check quote length to determine styling
+    if (vQuote.text.length > 100) {
+        mQuoteText.classList.add("long-quote");
+    } else {
+        mQuoteText.classList.remove("long-quote");
+    }
+    mQuoteText.textContent  = vQuote.text;
 }
 
 // Get and show quote locally
